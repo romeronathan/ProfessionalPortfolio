10d7b970f761851f515021a152a4022867f60064
nathan <romeronathan@hotmail.com>
2022-02-02 15:52:28 -0500

    Initial desing concept

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Link } from '../types/link';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent implements OnInit {
+  links: Link[] = [];
+  constructor() { }
+
+  ngOnInit(): void {
+    this.links = [
+      { name: 'Home', href: '/' },
+      { name: 'Work', href: '/about' },
+      { name: 'About', href: '/news-&-research' },
+      { name: 'Contact', href: '/contact' },
+    ];
+  }
+
+}
