4c48f08c094105ad4d6c50f0119d59f43b385a50
nathan <romeronathan@hotmail.com>
2022-02-03 17:21:46 -0500

    Project card start

--- src/app/pages/work/work.component.ts
+++ src/app/pages/work/work.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Project } from 'src/app/types/project';
 
 @Component({
   selector: 'app-work',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./work.component.scss']
 })
 export class WorkComponent implements OnInit {
-
+  projects: Project[] = [];
   constructor() { }
 
   ngOnInit(): void {
+    this.projects.push({
+      name: 'St. Clair Google Developer Student Club',
+      description: 'This is a project description',
+      tags: ['tag1', 'tag2'],
+      link: 'https://www.google.com',
+      img: '../../assets/images/onetenacity.JPG'
+    }
+    );
+    this.projects.push({
+      name: 'One Tenacity',
+      description: 'This is a project description',
+      tags: ['tag1', 'tag2'],
+      link: 'https://www.google.com',
+      img: '../../assets/images/stclair.JPG'
+    }
+    );
   }
 
 }
