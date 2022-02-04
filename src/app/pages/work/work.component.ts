e526d904c2c34e8710279e4856a1dce25330696e
nathan <romeronathan@hotmail.com>
2022-02-03 21:08:43 -0500

    Work on home page and work page

--- src/app/pages/work/work.component.ts
+++ src/app/pages/work/work.component.ts
@@ -20,6 +20,39 @@ export class WorkComponent implements OnInit {
     }
     );
     this.projects.push({
+      name: 'St. Clair Google Developer Student Club',
+      description: 'This is a project description',
+      tags: ['tag1', 'tag2'],
+      link: 'https://www.google.com',
+      img: '../../assets/images/onetenacity.JPG'
+    }
+    );
+    
+    this.projects.push({
+      name: 'One Tenacity',
+      description: 'This is a project description',
+      tags: ['tag1', 'tag2'],
+      link: 'https://www.google.com',
+      img: '../../assets/images/stclair.JPG'
+    }
+    );
+    this.projects.push({
+      name: 'One Tenacity',
+      description: 'This is a project description',
+      tags: ['tag1', 'tag2'],
+      link: 'https://www.google.com',
+      img: '../../assets/images/stclair.JPG'
+    }
+    );
+    this.projects.push({
+      name: 'One Tenacity',
+      description: 'This is a project description',
+      tags: ['tag1', 'tag2'],
+      link: 'https://www.google.com',
+      img: '../../assets/images/stclair.JPG'
+    }
+    );
+    this.projects.push({
       name: 'One Tenacity',
       description: 'This is a project description',
       tags: ['tag1', 'tag2'],
